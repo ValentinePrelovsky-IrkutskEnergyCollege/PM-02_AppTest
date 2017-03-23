e8a184bb87a14fe93db2882668302b35914ba0e4
Valentin <prelovskyvalentine@gmail.com>
2017-03-23 10:49:38 +0800

    + практика 2

---
+++ WindowsDebugApplication/WindowsDebugApplication/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace WindowsDebugApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FirstVariable = new System.Windows.Forms.TextBox();
+            this.SecondVariable = new System.Windows.Forms.TextBox();
+            this.SymbolcomboBox = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // FirstVariable
+            // 
+            this.FirstVariable.Location = new System.Drawing.Point(13, 30);
+            this.FirstVariable.Name = "FirstVariable";
+            this.FirstVariable.Size = new System.Drawing.Size(100, 20);
+            this.FirstVariable.TabIndex = 0;
+            // 
+            // SecondVariable
+            // 
+            this.SecondVariable.Location = new System.Drawing.Point(205, 30);
+            this.SecondVariable.Name = "SecondVariable";
+            this.SecondVariable.Size = new System.Drawing.Size(100, 20);
+            this.SecondVariable.TabIndex = 1;
+            // 
+            // SymbolcomboBox
+            // 
+            this.SymbolcomboBox.FormattingEnabled = true;
+            this.SymbolcomboBox.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "/"});
+            this.SymbolcomboBox.Location = new System.Drawing.Point(119, 29);
+            this.SymbolcomboBox.Name = "SymbolcomboBox";
+            this.SymbolcomboBox.Size = new System.Drawing.Size(80, 21);
+            this.SymbolcomboBox.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(119, 96);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(320, 266);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.SymbolcomboBox);
+            this.Controls.Add(this.SecondVariable);
+            this.Controls.Add(this.FirstVariable);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox FirstVariable;
+        private System.Windows.Forms.TextBox SecondVariable;
+        private System.Windows.Forms.ComboBox SymbolcomboBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsDebugApplication/WindowsDebugApplication/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections;
+namespace WindowsDebugApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private double c = 0;
+        private void Plus(double a, double b)
+        {
+            c = a + b;
+        }
+        private void Minus(double a, double b)
+        {
+            c = a - b;
+        }
+        private void Multiply(double a, double b)
+        {
+            c = a * b;
+        }
+        private void Split(double a, double b)
+        {
+            c = a / b;
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string symbol = (string)SymbolcomboBox.SelectedItem;
+            ArrayList arraysymbol = new ArrayList();
+            foreach (var s in SymbolcomboBox.Items)
+            {
+                arraysymbol.Add(s);
+            }
+            double var1 = Convert.ToDouble(FirstVariable.Text);
+            double var2 = Convert.ToDouble(SecondVariable.Text);
+            switch (symbol)
+            {
+                case "+": Plus(var1, var1); break;
+                case "-": Minus(var1, var1); break;
+                case "*": Multiply(var1, var1); break;
+                case "/": Split(var1, var1); break;
+            }
+            MessageBox.Show("Результат: " + c);
+            MessageBox.Show("Количество символов в коллекции: " + arraysymbol.Count);
+        }
+    }
+}

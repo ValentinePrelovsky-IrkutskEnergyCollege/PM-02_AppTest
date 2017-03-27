8ce1699b20acb23e99b1384790f8125d6d1d134c
Valentin <prelovskyvalentine@gmail.com>
2017-03-27 11:40:55 +0900

    SImple multtasking is ready

--- SimpleMultitaskingApp/SimpleMultitaskingApp/Program.cs
+++ SimpleMultitaskingApp/SimpleMultitaskingApp/Program.cs
@@ -2,13 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace SimpleMultitaskingApp
 {
     class Program
     {
-        static void Main(string[] args)
+        private static string printMessage(string message)
         {
+            return message.ToUpper();
         }
-    }
-}
+
+        static void Main(string[] args) 
+         {
+             Task<string[]> message = new Task<string[]>(() =>
+             {
+                 var result = new string[3];
+                 new Task(() => result[0] = printMessage("Hello"),TaskCreationOptions.AttachedToParent).Start();
+                 new Task(() => result[1] = printMessage("World"),TaskCreationOptions.AttachedToParent).Start();
+                 new Task(() => result[2] = printMessage("Hello world!"),TaskCreationOptions.AttachedToParent).Start();
+                 return result;
+             });
+             var cwt = message.ContinueWith(mes =>  Array.ForEach(mes.Result, Console.WriteLine));
+             message.Start();
+             Console.WriteLine("Главный метод завершен.");
+             Console.ReadLine(); 
+        } // eof main
+    } // eof class
+} // eof namespace
+
